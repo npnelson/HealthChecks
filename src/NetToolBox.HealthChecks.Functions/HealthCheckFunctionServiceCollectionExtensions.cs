fc914358a3a9b79e9d87edf433d8309339a9ec35
npnelson <nnelson1@insight.rr.com>
2020-05-12 17:42:11 -0400

    WIP

---
+++ src/NetToolBox.HealthChecks.Functions/HealthCheckFunctionServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using NetToolBox.HealthChecks.Functions;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class HealthCheckFunctionServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="HealthCheckService"/> to the container, using the provided delegate to register
+        /// health checks.
+        /// </summary>
+        /// <remarks>
+        /// This operation is idempotent - multiple invocations will still only result in a single
+        /// <see cref="HealthCheckService"/> instance in the <see cref="IServiceCollection"/>. It can be invoked
+        /// multiple times in order to get access to the <see cref="IHealthChecksBuilder"/> in multiple places.
+        /// </remarks>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the <see cref="HealthCheckService"/> to.</param>
+        /// <returns>An instance of <see cref="IHealthChecksBuilder"/> from which health checks can be registered.</returns>
+        public static IHealthChecksBuilder AddFunctionsHealthChecks(this IServiceCollection services)
+        {
+            services.TryAddSingleton<HealthCheckService, DefaultFunctionsHealthCheckService>();
+            return new HealthChecksFunctionBuilder(services);
+        }
+    }
+}
---
+++ src/NetToolBox.HealthChecks.Functions/HealthChecksFunctionBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+
+namespace NetToolBox.HealthChecks.Functions
+{
+    internal sealed class HealthChecksFunctionBuilder : IHealthChecksBuilder
+    {
+        public HealthChecksFunctionBuilder(IServiceCollection services)
+        {
+            Services = services;
+        }
+
+        public IServiceCollection Services { get; }
+
+        public IHealthChecksBuilder Add(HealthCheckRegistration registration)
+        {
+            if (registration == null)
+            {
+                throw new ArgumentNullException(nameof(registration));
+            }
+
+            Services.Configure<HealthCheckServiceOptions>(options =>
+            {
+                options.Registrations.Add(registration);
+            });
+
+            return this;
+        }
+    }
+}
