cc2cd3636ecd638a109623b6b0830177efbcdb4b
npnelson <nnelson1@insight.rr.com>
2020-05-13 10:09:32 -0400

    WIP

---
+++ test/NetToolBox.HealthChecks.Functions.TestHarness/Health.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Threading.Tasks;
+
+namespace NetToolBox.HealthChecks.Functions.TestHarness
+{
+    public class Health
+    {
+        private readonly HealthCheckService _healthCheckService;
+
+        public Health(HealthCheckService healthCheckService)
+        {
+            _healthCheckService = healthCheckService;
+        }
+        [FunctionName("Health")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req
+            )
+        {
+            var healthReport = await _healthCheckService.CheckHealthAsync();
+            return HealthReportObjectResult.GetHealthReportObjectResult(healthReport);
+        }
+    }
+}
