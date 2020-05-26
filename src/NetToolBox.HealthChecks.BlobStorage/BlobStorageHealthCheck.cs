a1cb25a073eba93486221e21c8532183f5229956
npnelson <nnelson1@insight.rr.com>
2020-05-26 15:48:54 -0400

    redact accountkey from blobhealthchecks

--- src/NetToolBox.HealthChecks.BlobStorage/BlobStorageHealthCheck.cs
+++ src/NetToolBox.HealthChecks.BlobStorage/BlobStorageHealthCheck.cs
@@ -2,6 +2,7 @@
 using NETToolBox.BlobStorage.Abstractions;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -27,22 +28,38 @@ namespace NetToolBox.HealthChecks.BlobStorage
                     var result = await _blobFactory.GetBlobStorage(container.accountName, container.containerName).IsHealthyAsync().ConfigureAwait(false);
                     if (result)
                     {
-                        itemDictionary.Add($"{container.accountName}:{container.containerName}", "OK");
+                        itemDictionary.Add($"{RedactAccountKey(container.accountName)}:{container.containerName}", "OK");
                     }
                     else
                     {
-                        itemDictionary.Add($"{container.accountName}:{container.containerName}", "HealthCheck returned unhealthy"); //this will probably never get a false, would more likely throw exception
+                        itemDictionary.Add($"{RedactAccountKey(container.accountName)}:{container.containerName}", "HealthCheck returned unhealthy"); //this will probably never get a false, would more likely throw exception
                         isHealthy = false;
                     }
                 }
                 catch (Exception ex)
                 {
                     isHealthy = false;
-                    itemDictionary.Add($"{container.accountName}:{container.containerName}", ex.ToString());
+                    itemDictionary.Add($"{RedactAccountKey(container.accountName)}:{container.containerName}", ex.ToString());
                 }
             }
             return new HealthCheckResult(isHealthy ? HealthStatus.Healthy : HealthStatus.Unhealthy, null, null, itemDictionary);
 
+        }
+
+        private string RedactAccountKey(string accountName)
+        {
+            var retval = accountName;
+
+            var segments = retval.Split(';');
+            for (var counter = 0; counter < segments.Count(); counter++)
+            {
+                if (segments[counter].StartsWith("AccountKey", StringComparison.OrdinalIgnoreCase))
+                {
+                    segments[counter] = "AccountKey=<redacted>";
+                }
+            }
+            retval = string.Join(";", segments);
+            return retval;
         }
     }
 }
